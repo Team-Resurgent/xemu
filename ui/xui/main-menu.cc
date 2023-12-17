a743688956274c3b45fa3213f385a94aa5395b0e
EqUiNoX <87106037+EqUiNoX-Labs@users.noreply.github.com>
2023-12-16 20:20:27 -0800

    latest changes
    * ui: Use only one option for settings window (#1122)
    
    * rebase code
    
    * remove unsused item
    
    * restore "system" displaying on first boot
    
    * restore popup menu functions (separate commit)
    
    * restore snapshot function in popup menu
    
    * get current index value from config file
    
    * ui: Warn user about no output when AV Pack set to "None"
    
    ---------
    
    Co-authored-by: Fabx <30447649+Fabxx@users.noreply.github.com>

--- ui/xui/main-menu.cc
+++ ui/xui/main-menu.cc
@@ -989,6 +989,10 @@ void MainMenuSystemView::Draw()
         ImGui::TextColored(ImVec4(1,0,0,1), "Application restart required to apply settings");
     }
 
+    if ((int)g_config.sys.avpack == CONFIG_SYS_AVPACK_NONE) {
+        ImGui::TextColored(ImVec4(1,0,0,1), "Setting AV Pack to NONE disables video output.");
+    }
+
     SectionTitle("System Configuration");
 
     if (ChevronCombo(
@@ -1184,34 +1188,21 @@ MainMenuScene::MainMenuScene()
     m_next_view_index = m_current_view_index;
 }
 
-void MainMenuScene::ShowGeneral()
+void MainMenuScene::ShowSettings()
 {
-    SetNextViewIndexWithFocus(0);
+    SetNextViewIndexWithFocus(g_config.general.last_viewed_menu_index);
 }
-void MainMenuScene::ShowInput()
-{
-    SetNextViewIndexWithFocus(1);
-}
-void MainMenuScene::ShowDisplay()
-{
-    SetNextViewIndexWithFocus(2);
-}
-void MainMenuScene::ShowAudio()
-{
-    SetNextViewIndexWithFocus(3);
-}
-void MainMenuScene::ShowNetwork()
-{
-    SetNextViewIndexWithFocus(4);
-}
-void MainMenuScene::ShowSnapshots() 
+
+void MainMenuScene::ShowSnapshots()
 {
     SetNextViewIndexWithFocus(5);
 }
+
 void MainMenuScene::ShowSystem()
 {
     SetNextViewIndexWithFocus(6);
 }
+
 void MainMenuScene::ShowAbout()
 {
     SetNextViewIndexWithFocus(7);
